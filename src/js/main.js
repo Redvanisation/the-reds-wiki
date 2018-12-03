ed3116433c0b42c2de31325e6313846dafde4f63
Redvanisation <red@macs-MacBook-Pro-2.local>
2018-12-03 15:59:12 -0500

    adding full functionality with es6

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,55 @@
 const init = () => {
     
+    // GETTING THE NEEDED HTML ELEMENTS FOR LATER USE:
     const input = document.querySelector('#input');
     const submit = document.querySelector('#btn-search');
+    const content = document.querySelector('#content-div');
 
-    submit.addEventListener('click', () => {
+    // DECLARING A FUNCTION TO HANDLE THE API CALL AND SHOW ITS DATA:
+    const getData = () => {
+
+        // GETTING THE INPUT VALUE:
         let keyword = input.value;
+
+        // DELETING THE INPUT DATA AFTER EACH SEARCH:
         input.value = '';
 
-        let api = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${keyword}&format=json&callback=?`;
+        // DELETING THE HTML RESULTS AFTER EACH NEW SEARCH:
+        content.innerHTML = '';
 
-        fetch(api, {// mode: "no-cors",
-            method: 'get',
-            headers: {
-            "Content-Type": "application/json; charset=UTF-8"}
-        })
+        // DECLARING THE API URL LINK:
+        const api = `https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search=${keyword}`;
+
+        // FETCHING THE API DATA FROM THE API SERVER:
+        fetch(api)
         .then((data) => data.json())
-        .then((resp) => console.log(resp));
-    });
+        .then((resp) => {
+
+            // LOOPING THROUGH THE RESULTS AND SHOWING EACH PIECE ON ITS RELATED HTML TAG:
+            for (let i in resp) {
+                content.innerHTML += `
+                <div class="content-section__second--result-container">
+                    <hr class="content-section__second--hr">
+                    <h2 class="content-section__second--h2">${resp[1][i]}</h2>
+                    <p class="content-section__second--text">
+                    ${resp[2][i]}
+                    <a href="${resp[3][i]}" class="content-section__second--link" target="_blank">Read more...</a>
+                    </p>
+                </div>`;
+            }
+        })
+        .catch((err) => console.log(err));
+    }
+
+    // CALLING THE GET DATA FUNCTION ON EACH CLICK OF THE SUBMIT BUTTON:
+    submit.addEventListener('click', () => getData());
+
+    // CALLING THE GET DATA FUNCTION ON EACH PRESS OF ENTER KEY:
+    input.addEventListener('keypress', (e) => {
+        if (e.which == 13) {
+            getData();
+        }
+    })
     
 
     
